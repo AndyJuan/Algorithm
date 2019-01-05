6ab0f3b6921dbeade8d62fe01455b6d4fcb45f77
刘娟 <1937331536@qq.com>
2019-01-05 20:25:45 +0800

    no message

--- OfferCode/src/code_31_02_PrintFromTopToBotton.java
+++ OfferCode/src/code_31_02_PrintFromTopToBotton.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 /**
  * 题目描述
  * 从上到下按层打印二叉树，同一层结点从左至右输出。每一层输出一行。
@@ -8,6 +11,39 @@
 
 
 public class code_31_02_PrintFromTopToBotton {
-    
+    /**
+     * 把二叉树打印成多行
+     * @param pRoot 二叉树根节点
+     * @return 结果list
+     */
+    ArrayList<ArrayList<Integer> > Print(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        if (pRoot == null) {
+            return list;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(pRoot);
+        int cnt = 1;
+        while (cnt > 0) {
+            int num = cnt;
+            cnt = 0;
+            ArrayList<Integer> res = new ArrayList<>();
+            for (int i = 0; i < num; ++i) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                    ++cnt;
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                    ++cnt;
+                }
+                res.add(node.val);
+            }
+            list.add(res);
+        }
+        return list;
+    }
 
 }
