4c60a3b966da268757d8d1f1ee449bce447662e1
刘娟 <1937331536@qq.com>
2019-03-13 17:01:58 +0800

    no message

--- Forexercise/src/code_61_pukpai.java
+++ Forexercise/src/code_61_pukpai.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class code_61_pukpai {
     public boolean isContinuous(int [] numbers){
+        if (numbers.length<5){
+            return false;
+        }
+
+        Arrays.sort(numbers);
+        int cnt = 0;
+        for (int num : numbers){
+            if (num == 0 ){
+                cnt++;
+            }
+        }
+        for(int i =cnt; i < numbers.length-1; i++){
+            if (numbers[i+1] ==numbers[i]){
+                return false;
+            }
+            cnt -= numbers[i+1] - numbers[i] -1;
+        }
+        return cnt>=0;
+
+
 
     }
 
